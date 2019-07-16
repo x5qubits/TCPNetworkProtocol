7c30b437216084bbc4059561516c1bdd026a80dc
Icyteck01 <52882756+Icyteck01@users.noreply.github.com>
2019-07-16 12:02:33 +0300

    Tests

---
+++ Tests/Client/Program.cs
@@ -0,0 +1,142 @@
+﻿using JHSNetProtocol;
+using System;
+using System.Timers;
+
+namespace TEST
+{
+    public enum SearchMatchOperations
+    {
+        NO,
+        Search,
+        START
+    }
+
+    public class SearchMatch : JHSMessageBase
+    {
+        public SearchMatchOperations op;
+        public uint value;
+        public string IP = "";
+        public short port = 0;
+
+        public override void Deserialize(JHSNetworkReader reader)
+        {
+            op = (SearchMatchOperations)reader.ReadByte();
+            if (op == SearchMatchOperations.Search)
+            {
+                value = reader.ReadPackedUInt32();
+            }
+            if (op == SearchMatchOperations.START)
+            {
+                IP = reader.ReadString();
+                port = reader.ReadInt16();
+            }
+        }
+
+        public override void Serialize(JHSNetworkWriter writer)
+        {
+            writer.Write((byte)op);
+            if (op == SearchMatchOperations.Search)
+            {
+                writer.WritePackedUInt32(value);
+            }
+
+            if (op == SearchMatchOperations.START)
+            {
+                writer.Write(IP);
+                writer.Write(port);
+            }
+        }
+    }
+
+    class Program
+    {
+        private static Timer timer1;
+        private static Timer timer2;
+
+        static void Main(string[] args)
+        {
+            NetConfig.logFilter = JHSLogFilter.Developer;
+            JHSNetworkClient.RegisterHandler(InternalMessages.CONNECTED, CONNECTED_TO_SERVER);
+            JHSNetworkClient.RegisterHandler(InternalMessages.DISCONNECT, DISCONNECTED_PERMANENT);
+            JHSNetworkClient.RegisterHandler(InternalMessages.DISCONNECT_BUT_WILL_RECONNECT, DISCONNECTED_FROM_SERVER);
+            JHSNetworkClient.RegisterHandler(100, TESTMSGREC);
+            JHSNetworkClient.Start("127.0.0.1");
+            timer1 = new Timer();
+            timer1.Elapsed += OnTimedEvent;
+            timer1.Interval = 5000; // in miliseconds
+            timer1.Start();
+            timer2 = new Timer();
+            timer2.Elapsed += SendPackets;
+            timer2.Interval = 100; // in miliseconds
+            timer2.Start();
+            Console.WriteLine("Press c to stop it and any key to send msg");
+            bool loop = true;
+            while (loop == true)
+            {
+               
+                string key = Console.ReadLine();
+                switch (key)
+                {
+                    case "c":
+                        loop = false;
+                        break;
+                    default:
+                        JHSNetworkClient.Send(100, new SearchMatch() { op = SearchMatchOperations.NO,  value = 0 });
+                        Console.WriteLine("SENT");
+                        break;
+                }
+               
+            }
+
+            
+        }
+
+        private static void DISCONNECTED_PERMANENT(JHSNetworkMessage netMsg)
+        {
+            Console.WriteLine("DISCONNECTED_PERMANENT");
+        }
+
+        private static void SendPackets(object sender, ElapsedEventArgs e)
+        {
+            JHSNetworkClient.Update();
+        }
+
+        private static float Time = 0;
+        private static void TESTMSGREC(JHSNetworkMessage netMsg)
+        {
+            SearchMatch msg = netMsg.ReadMessage<SearchMatch>();
+            if(msg != null)
+            {
+                    Time = JHSTime.Time + 5;
+                    Console.WriteLine(msg.op.ToString());
+            }
+        }
+
+        private static void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            
+
+            if (JHSNetworkClient.Connected)
+                JHSNetworkClient.Send(100, new SearchMatch() { op = SearchMatchOperations.NO });
+        }
+
+
+        private static void DISCONNECTED_FROM_SERVER(JHSNetworkMessage netMsg)
+        {
+            Console.WriteLine("DISCONNECTED_FROM_SERVER");
+        }
+
+        private static void CONNECTED_TO_SERVER(JHSNetworkMessage netMsg)
+        {
+
+            if (netMsg.conn != null)
+            {
+                Console.Title = "Connection:" + netMsg.conn.connectionId;
+                Console.WriteLine("CONNECTED_TO_SERVER CONNECTION ID:"+ netMsg.conn.connectionId);
+            }
+            else
+                Console.WriteLine("CONNECTED_TO_SERVER");
+
+        }
+    }
+}
---
+++ Tests/Server/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Timers;
+
+namespace JHSNetProtocol
+{
+    public enum SearchMatchOperations
+    {
+        NO,
+        Search,
+        START
+    }
+
+    public class SearchMatch : JHSMessageBase
+    {
+        public SearchMatchOperations op;
+        public uint value;
+        public string IP = "";
+        public short port = 0;
+
+        public override void Deserialize(JHSNetworkReader reader)
+        {
+            op = (SearchMatchOperations)reader.ReadByte();
+            if (op == SearchMatchOperations.Search)
+            {
+                value = reader.ReadPackedUInt32();
+            }
+            if (op == SearchMatchOperations.START)
+            {
+                IP = reader.ReadString();
+                port = reader.ReadInt16();
+            }
+        }
+
+        public override void Serialize(JHSNetworkWriter writer)
+        {
+            writer.Write((byte)op);
+            if (op == SearchMatchOperations.Search)
+            {
+                writer.WritePackedUInt32(value);
+            }
+
+            if (op == SearchMatchOperations.START)
+            {
+                writer.Write(IP);
+                writer.Write(port);
+            }
+        }
+    }
+
+    class Program
+    {
+        private static Timer timer1;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "JHS Server Gateway";
+            NetConfig.logFilter = JHSLogFilter.Developer;
+            JHSNetworkServer.RegisterHandler(100, TESTMSG_RECIVE);
+            JHSNetworkServer.Start("0.0.0.0");
+            timer1 = new Timer();
+            timer1.Elapsed += OnTimedEvent;
+            timer1.Interval = 200; // in miliseconds
+            timer1.Start();
+            Console.ReadKey();
+        }
+
+        private static void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+           // JHSNetworkServer.SendToAll(100, new SearchMatch() { IP = "127.0.0.1", port = 1985 });
+        }
+
+
+        private static void TESTMSG_RECIVE(JHSNetworkMessage netMsg)
+        {
+            uint connectionId = netMsg.conn.connectionId;
+            string ip = netMsg.conn.IP;
+            SearchMatch packet = netMsg.ReadMessage<SearchMatch>();
+            if(packet != null)
+            {
+                JHSDebug.Log(packet.op.ToString());
+                netMsg.conn.Send(100, new SearchMatch() { op = SearchMatchOperations.Search });
+                netMsg.conn.Send(100, new SearchMatch() { op= SearchMatchOperations.START, IP = "127.0.0.1", port = 1985 });
+
+              //  JHSNetworkManager.SendToAll(100, new TESTMSGRE() {ClientId = connectionId, Time = packet.Time, TimeServ = JHSTime.TimeStamp });
+            }
+        }
+    }
+}
